1de0b3c380bb0e6fc9adb4f7118fed99c15ecc97
NtwaliEliel <you@example.com>
2024-07-20 03:05:54 -0700

    100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,40 +1,40 @@
-#!/usr/bin/env bash
-# 100-puppet_ssh_config.pp
-# Ensure the SSH configuration directory exists
-file { '/home/ubuntu/.ssh':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0700',
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-# Ensure the SSH config file exists
-file { '/home/ubuntu/.ssh/config':
-  ensure  => present,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-  content => '',
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $      end of the line
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-# Add configuration to use the private key ~/.ssh/school
-file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^\\s*IdentityFile\\s+',
-  notify => Exec['reload-ssh'],
-}
-
-# Add configuration to refuse to authenticate using a password
-file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
-  line  => '    PasswordAuthentication no',
-  match => '^\\s*PasswordAuthentication\\s+',
-  notify => Exec['reload-ssh'],
-}
-
-# Reload SSH service (if necessary)
-exec { 'reload-ssh':
-  command     => '/usr/bin/true',
-  refreshonly => true,
-}
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line
